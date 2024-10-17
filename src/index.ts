aaa97413e77551e58f6181f406979314379439ec
JuanJo Lurbe <juanjo.lurbe@gmail.com>
2024-10-18 00:26:32 +0200

    feat: added nodemon and dotenv deps. Added config file for managing the env vars

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import { add } from './utils'
 
-dotenv.config()
+console.log('sum', add(1, 2))
 
-const app = express()
-const port = process.env.PORT || 3000
-if (port === 3000) console.log(`Listening on port ${port}`)
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+console.log('debug', process.env.APP_DEBUG)
